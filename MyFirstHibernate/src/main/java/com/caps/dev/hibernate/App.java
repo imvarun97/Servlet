33dbb61a5eb8366b9eb140eea668567cf02e7cc6
varun <varun@varun-pc>
2018-10-29 19:19:03 +0530

    updated app.java

--- MyFirstHibernate/src/main/java/com/caps/dev/hibernate/App.java
+++ MyFirstHibernate/src/main/java/com/caps/dev/hibernate/App.java
@@ -12,6 +12,7 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 import com.caps.dev.beans.Movie;
+import com.caps.model.JPAUtils;
 
 /**
  * Hello world!
@@ -21,6 +22,14 @@ public class App
 {
 	public static void main( String[] args )
 	{
+		//save();
+		//getMovieById();
+		//remove();
+		//getMovieById();
+		Update();
+	}
+	
+	public static void save() {
 		System.out.println("Enter Movie Details");
 		System.out.println("-------------------");
 		Movie movie = new Movie();
@@ -37,16 +46,69 @@ public class App
 
 		System.out.println("Enter movie summary: ");
 		movie.setSummmary(in.nextLine());
-		save(movie);
 		System.out.println("Movie Saved");
-	}
-	
-	public static void save(Movie movie) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("myPersistanceUnit");
+		EntityManagerFactory emf = JPAUtils.getEMF();
 		EntityManager em = emf.createEntityManager();
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
 		em.persist(movie);
 		tx.commit();
+		em.close();
+			
+		in.close();
+	}
+	
+	public static void getMovieById() {
+		System.out.println("Enter a movie id: ");
+		Scanner sc = new Scanner(System.in);
+		int id = Integer.parseInt(sc.nextLine());
+		EntityManagerFactory emf = JPAUtils.getEMF();
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		Movie m = em.getReference(Movie.class, id);
+		tx.commit();
+		em.close();
+		emf.close();
+		sc.close();
+		System.out.println(m);
+	}
+	public static void remove()
+	{
+		System.out.println("Enter a movie id to del: ");
+		Scanner sc = new Scanner(System.in);
+		int id = Integer.parseInt(sc.nextLine());
+		EntityManagerFactory emf = JPAUtils.getEMF();
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		Movie m = em.getReference(Movie.class, id);
+		em.remove(m);
+		tx.commit();
+		em.close();
+		emf.close();
+		sc.close();
+		System.out.println(m);
+	}
+	public static void Update()
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a movie id to update: ");
+		int id = Integer.parseInt(sc.nextLine());
+		System.out.println("Enter ratings: ");
+		Double ratings = Double.parseDouble(sc.nextLine());
+		EntityManagerFactory emf = JPAUtils.getEMF();
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		Movie m = em.find(Movie.class, id);
+		m.setRatings(ratings);
+		
+		tx.commit();
+		em.close();
+		emf.close();
+		sc.close();
+		System.out.println(m);
 	}
 }
